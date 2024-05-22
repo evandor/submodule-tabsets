7257f97b8a05c58e25f396cd9e2a0bff7cacde00
Carsten Gräf <evandor@gmail.com>
2024-05-22 12:59:34 +0200

    submodules refactoring

---
+++ models/SaveOrReplaceResult.ts
@@ -0,0 +1,11 @@
+import {Tabset} from "src/tabsets/models/Tabset";
+
+export class SaveOrReplaceResult {
+
+  constructor(
+    public replaced: boolean,
+    public tabset: Tabset,
+    public merged: boolean
+  ) {}
+
+}
