121028002542343e488d65d0b8bd2e4c982067d2
Carsten Gräf <evandor@gmail.com>
2024-08-07 10:09:27 +0200

    cleanups

--- widgets/PanelTabListElementWidget.vue
+++ widgets/PanelTabListElementWidget.vue
@@ -364,9 +364,9 @@ import TabDetailsSearchIndex from "pages/sidepanel/helper/TabDetailsSearchIndex.
 import {DeleteCommentCommand} from "src/domain/tabs/DeleteCommentCommand";
 import {UpdateTabNameCommand} from "src/domain/tabs/UpdateTabName";
 import {SavedBlob} from "src/snapshots/models/SavedBlob";
-import TabListIconIndicatorsHook from "components/hooks/TabListIconIndicatorsHook.vue";
 import {useThumbnailsService} from "src/thumbnails/services/ThumbnailsService";
 import CommentDialog from "src/tabsets/dialogues/CommentDialog.vue";
+import TabListIconIndicatorsHook from "src/app/hooks/tabsets/TabListIconIndicatorsHook.vue";
 
 const {inBexMode} = useUtils()
 
